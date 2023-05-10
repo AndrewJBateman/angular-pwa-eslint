220630d4c4639cdeaea5bb3d8397b1e66db6471d
AndrewJBateman <gomezbateman@yahoo.com>
2023-05-10 21:20:02 +0200

    refactor: rem modules, add config. factory access

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { SettingsService } from "./settings.service";
 
 @Component({
 	selector: "app-root",
@@ -7,4 +8,8 @@ import { Component } from "@angular/core";
 })
 export class AppComponent {
 	title = "angular-pwa-eslint";
+
+  constructor(private settingsService: SettingsService) {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,69 @@
-import { NgModule } from "@angular/core";
+import { APP_INITIALIZER, NgModule, inject } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { ServiceWorkerModule } from "@angular/service-worker";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 
 import { environment } from "../environments/environment";
-import { DashboardModule } from "./modules/application/dashboard/dashboard.module";
-import { StocksModule } from './modules/application/stocks/stocks.module';
+import { NavModule } from "./modules/common/nav/nav.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { SettingsService } from "./settings.service";
+import { catchError, tap } from "rxjs/operators";
+import { of } from "rxjs";
 
 @NgModule({
 	declarations: [AppComponent],
+	providers: [
+		{
+			provide: APP_INITIALIZER,
+			useFactory: () => {
+				const settingsService = inject(SettingsService);
+				const http = inject(HttpClient);
+				return () =>
+					new Promise((resolve) => {
+						// load settings for a deployed App
+						if (environment.production) {
+							http
+								.get("./config.json")
+								.pipe(
+									tap((data: any) => {
+										settingsService.baseUrl = data.baseUrl;
+										resolve(true);
+									}),
+									catchError((error) => {
+										settingsService.baseUrl = "http://default.api";
+										resolve(true);
+										return of(null);
+									})
+								)
+								.subscribe();
+						} else {
+							// load settings for local App
+							const settings = require("../../config.json");
+							settingsService.baseUrl = settings.baseUrl;
+              console.log('settings: ', settingsService.baseUrl);
+							resolve(true);
+						}
+					});
+			},
+			multi: true
+		}
+	],
+	bootstrap: [AppComponent],
 	imports: [
-		BrowserModule.withServerTransition({ appId: "serverApp" }),
+		BrowserModule,
 		AppRoutingModule,
-    HttpClientModule,
+		HttpClientModule,
 		ServiceWorkerModule.register("ngsw-worker.js", {
 			enabled: environment.production,
 			// Register the ServiceWorker as soon as the app is stable
 			// or after 30 seconds (whichever comes first).
 			registrationStrategy: "registerWhenStable:30000"
 		}),
-		DashboardModule,
-  StocksModule
-	],
-	providers: [],
-	bootstrap: [AppComponent]
+		NavModule,
+		BrowserAnimationsModule
+	]
 })
 export class AppModule {}
